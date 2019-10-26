2c7c2385e8f6163cbe5c118c671bc097e8cdd109
唐婷 <tangting@tangtingdeAir.lan>
2019-10-26 16:33:37 +0800

    两数之和

--- subjects/1.the sums of two nums.js
+++ subjects/1.the sums of two nums.js
@@ -1,22 +1,3 @@
-# leetcode-practice(争取每日1题1024)
-
-## 1. 算法
-
-### 1. 两数之和
-
-给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
-
-示例：
-```text
-给定 nums = [2, 7, 11, 15], target = 9
-
-因为 nums[0] + nums[1] = 2 + 7 = 9
-所以返回 [0, 1]
-```
-
-我当时的解法：
-
-```js
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -34,10 +15,10 @@ var twoSum = function(nums, target) {
     console.log(arr);
     return arr;
 };
-```
 
-其他题解1
-```js
+
+
+// 其他题解1
 // 暴力法
 // 使用两层循环，外层循环计算当前元素与 targettarget 之间的差值，内层循环寻找该差值，若找到该差值，则返回两个元素的下标。
 // 时间复杂度：O(n^2)
@@ -58,29 +39,9 @@ var twoSum = function(nums, target) {
         }
     }
 };
-```
 
 // 其他题解2
-```js
 // 利用数组减少查询时间
 // 在暴力法中，内层循环查找差值很浪费时间，那么如何减少查询时间呢？利用数组就可以减少查询时间。
 // 使用一层循环，每遍历到一个元素就计算该元素与 targettarget 之间的差值 difdif，然后以 difdif 为下标到数组temp中寻找，如果 temp[dif] 有值(即不是 undefinedundefined)，则返回两个元素在数组 numsnums 的下标，如果没有找到，则将当前元素存入数组 temptemp 中(下标: nums[i], Value: inums[i],Value:i) 。
 // 时间复杂度：O(n)
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function(nums, target) {
-    var temp = [];
-    for(var i=0;i<nums.length;i++){
-        var dif = target - nums[i];
-        if(temp[dif] != undefined){
-            return [temp[dif],i];
-        }
-        temp[nums[i]] = i;
-    }
-};
-```
-
-
